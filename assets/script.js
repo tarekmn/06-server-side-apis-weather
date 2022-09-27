a232e6ff06a23b8c3d7e19d3c009bc8f891f270b
tarekmn <nytarek@gmail.com>
2022-09-27 17:16:41 -0500

    mvp

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,99 @@
+//Global Variables
+let dataArray = []
+let localStorageArray = []
+const now = moment().format('l');
+
+//Element Selectors 
 let searchButton = document.getElementById('searchButton');
 let input= document.getElementById('formInput')
-// let notSure = document.getElementById('.notSure');
+let appendHere = document.getElementById('whereToAppend');
 let mainWeather = document.getElementById("apiHeader");
 let mainTemp = document.getElementById('temp');
 let mainWind = document.getElementById('wind');
 let mainHumidity = document.getElementById('humidity');
-
-
-const weatherDays = []
-let currDay = null
+let mainselector = document.getElementsByTagName('main');
 
 
 
+
+//Function to console log saved cities, will later update
+function retrieveArray() {
+  var locate = JSON.parse(localStorage.getItem("city"))
+  console.log(locate)
+}
+
+
+
+//Primary getApi function
 function getApi(event) {
   var city = input.value
   var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=188a51d5f423814817d69f7e3e5961b3`;
 
-
+  console.log(localStorageArray);
 
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // Use the console to examine the response
+
+      console.log(data)
+
+      // This is the main page display
+      let city = data.city.name
       let temp= ((data.list[0].main.temp-273.15)*1.8)+32
+      let wind= data.list[0].wind.speed
+      let humidity = data.list[0].main.humidity
+      let dataArray = []
 
-      console.log(data);
-      console.log(data.city.name) //city name
-      console.log(temp) // temp not sure why so high
-      console.log(data.list[0].wind.speed) //wind mph
-      console.log(data.list[0].main.humidity) //humidity %
-
-      mainWeather.textContent = data.city.name;
+      //assigning the values to main page
+      mainWeather.textContent = city + " " + now;
       mainTemp.textContent = "Temp: " + temp.toFixed(2) + " °F";
-      mainWind.textContent = "Wind: " + data.list[0].wind.speed + " MPH";
-      mainHumidity.textContent = "Humidity: " + data.list[0].main.humidity + "%";
+      mainWind.textContent = "Wind: " + wind + " MPH";
+      mainHumidity.textContent = "Humidity: " + humidity + "%";
+
+      //Turning 5day 3hour api into 5 day 24 hour
+      dataArray.push(data.list[1])
+      dataArray.push(data.list[9])
+      dataArray.push(data.list[17])
+      dataArray.push(data.list[25])
+      dataArray.push(data.list[33])
+      console.log(dataArray)
+
+      //DOM -appending 5 days
+      for (let index = 0; index < dataArray.length; index++) {
+        //console.log(index)
+
+        let apiHeaderi = document.createElement('h6');
+        let tempi = document.createElement('p');
+        let windi = document.createElement('p');
+        let humidityi = document.createElement('p');
+        let tempy = ((dataArray[index].main.temp-273.15)*1.8)+32
+
+        apiHeaderi.textContent= moment().add(index +1, 'days').format('l') //date
+        tempi.textContent= "Temp: " + tempy.toFixed(2) + " °F"
+        windi.textContent= "Wind: " + dataArray[index].wind.speed+ " MPH"
+        humidityi.textContent= "Humidity: " + dataArray[index].main.humidity + "%"
+
+        document.body.appendChild(apiHeaderi);
+        document.body.appendChild(tempi);
+        document.body.appendChild(windi);
+        document.body.appendChild(humidityi);
+
+      }
+      // //Reseting Values so there it replaces new 5 days instead of add
+      // apiHeaderi.textContent= ""
+      // tempi.textContent= ""
+      // windi.textContent= ""
+      // humidityi.textContent= ""
 
 
-
-
-
-
-      // // TODO: Loop through the data and generate your HTML
-      // for (let index = 0; index < data.length; index++) {
-      //   var loginName = document.createElement('h3');
-      //   var url = document.createElement('p');
-      //   loginName.textContent = data[i].user.login;
-      //   url.textContent = data[i].url;
-      //   userContainer.appendChild(loginName)
-      //   userContainer.appendChild(url)
-
-
-
+      //Creating a new array of saved cities, pushing to local storage. Then retrieving
+      localStorageArray.push(city)
+      localStorage.setItem("city", JSON.stringify(localStorageArray));
+      
+      retrieveArray()
+      
     });
 
 
@@ -60,24 +101,5 @@ function getApi(event) {
 }
 
 
-// const weatherDays = []  
-// let currDay = null
 
-// sampleData.list.forEach( function(tsObj){
-
-//   // Makes a moment date object for each record
-//   const dateObj = moment.unix(tsObj.dt)
-
-//   // Generate the day # for the day in the date object
-//   const dateNum = dateObj.format("DDD")
-
-//   // If the current date in tsObj hasn't had a record put into weatherDays, do that now 
-//   // Then skip over all other records for this day
-//   if( dateNum !== currDay && weatherDays.length < 5 ){
-//     weatherDays.push( tsObj )
-//     currDay = dateNum
-//   }
-
-// })
-
-searchButton, addEventListener('click', getApi);
+searchButton.addEventListener('click', getApi);
